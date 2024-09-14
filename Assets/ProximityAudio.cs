729b4ad6191693a8b92fa4ce884ab484805a7ad4
Alan Wang <alanwang093@gmail.com>
2024-09-14 14:39:39 -0400

    added TextCard to pedestal prefab

---
+++ Assets/ProximityAudio.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ProximityAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Stop();
+        }
+    }
+}
+
